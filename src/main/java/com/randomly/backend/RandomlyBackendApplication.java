9bb8ca2d3cdced3647a9eb0b299ea560fee06ca6
Punkil Baliyan <112774489+PunkilBaliyan@users.noreply.github.com>
2026-01-17 15:33:48 +0530

    first

---
+++ src/main/java/com/randomly/backend/RandomlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.randomly.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomlyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/randomly/backend/RandomlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
