99473f26a98693087ed8e6186acace734e250866
Punkil Baliyan <112774489+PunkilBaliyan@users.noreply.github.com>
2026-01-17 22:02:20 +0530

    commit 2
    backend socket connected but not working

---
+++ src/main/java/com/randomly/ws/TestSocketController.java
@@ -0,0 +1,15 @@
+package com.randomly.ws;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestSocketController {
+
+    @MessageMapping("/ping")
+    @SendTo("/topic/pong")
+    public String ping(String message) {
+        return "pong";
+    }
+}
